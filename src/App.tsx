303d24b9ed54f55723fbcf31f6564e3fb21d39c2
Daisuke Motohashi <ddssk.m@gmail.com>
2025-11-01 17:25:45 +0900

    Update index.html and App.tsx for asset path adjustments
    - Changed the script source in index.html to use the updated JavaScript file.
    - Modified the BrowserRouter in App.tsx to set the basename for routing to "/trial-tanstack".

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const queryClient = new QueryClient({
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <BrowserRouter>
+      <BrowserRouter basename="/trial-tanstack">
         <Layout>
           <Routes>
             <Route path="/" element={<Home />} />
